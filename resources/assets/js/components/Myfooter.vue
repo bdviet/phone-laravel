653c9f38fd3ddc6c2501b9c6ac60ed0f3141d636
v-bui <v-bui@dimage.co.jp>
2018-07-27 08:56:49 +0700

    done layout

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,13 @@
+<template>
+	<footer class="footer">
+		<div class="content has-text-centered">
+			<p>
+				<strong>Vuejs Phonebook App</strong> by <a href="https://facebook.com/bdviett">Bui Duc Viet</a>. The source code is licensed
+				<a href="https://github.com/bdviet/phone-laravel">Phonebook</a>
+				<a class="icon" href="https://github.com/bdviet/">
+					<i class="fa fa-github"></i>
+				</a>
+			</p>
+		</div>
+	</footer>
+</template>
